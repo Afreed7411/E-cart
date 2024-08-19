78031959f6c24ca8acd51d28759a07dd27383835
Afreed <afreedmokashi@gmail.com>
2024-08-19 18:22:31 +0530

    first commit

---
+++ practice of react/my-test/src/assets/data.js
@@ -0,0 +1,41 @@
+export const data =[
+    {
+        question:"Which device is required for the network?",
+        option1:"Modem",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans: 1,
+    },
+    {
+        question:"which continents has heighets? ",
+        option1:"Asia",
+        option2:"Europe",
+        option3:"North America ",
+        option4:"Africa",
+        ans: 4,
+    },
+    {
+        question:"A computer cannot BOOT if it does have the ?",
+        option1:"Application Software ",
+        option2:"Internet",
+        option3:"Operatning System",
+        option4:"Mouse",
+        ans: 2,
+    },
+    {
+        question:"first page of website is termed as ?",
+        option1:"Index page",
+        option2:"Homepage",
+        option3:"site map",
+        option4:"pan drive",
+        ans: 2,
+    },  {
+        question:"full form of HTML?",
+        option1:"Hyper text mark lanugage",
+        option2:"hyper",
+        option3:"jwdhxuewhbody",
+        option4:"nwdcnewj",
+        ans: 1.
+    },
+]
